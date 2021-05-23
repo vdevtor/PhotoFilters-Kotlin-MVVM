b8503209fcda421b6b730fd73f602163cbcbb944
Vitor Soares Silva <vitaoosp@gmail.com>
2021-05-22 22:26:53 -0300

    add edit image activity and creating its layout

---
+++ app/src/main/java/com/example/photofilterapp/EditImageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.photofilterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_image)
+    }
+}
